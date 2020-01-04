8fa264dcf9fdea7151f865b19a3be9dc4304a291
Shahzin <shahzin64@gmail.com>
2020-01-04 18:27:35 +0530

    hey

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 import requests
 import os
 import csv
+import json
 #import urllib.request
 
 
@@ -19,8 +20,45 @@ def jsonParser():
     #html = response.read()
 
     r = requests.get(download_url)
-    return "successfully downloaded"
+    with open("testerss.json","wb") as f:
+        f.write(r.content)
 
+    with open('testerss.json') as f:
+        x = json.load(f)
+
+    f = csv.writer(open("jsontocsvconverted.csv", "w", newline=''))
+
+    f.writerow(["id", "phones", "email", "firstname", "lastname","role","username","isActive","_created_at","_updated_at"])
+
+    for x in x:
+        try:
+            f.writerow([x["id"],
+                        x["phones"],
+                        x["email"],
+                        x["firstname"],
+                        x["lastname"],
+                        x["role"],
+                        x["username"],
+                        x["isActive"],
+                        x["_created_at"],
+                        x["_updated_at"]])
+        except:
+            f.writerow([x["_id"],
+                        x["phones"],
+                        x["email"],
+                        x["firstname"],
+                        x["lastname"],
+                        x["role"],
+                        x["username"],
+                        x["isActive"],
+                        x["_created_at"],
+                        x["_updated_at"]])
+
+    #return send_from_directory(directory='/smartservprojects',filename='testlol.csv', as_attachment=True)
+    #return "downloading.."
+    #return send_file('/Users/shahzinsajid/SmartServProjects/testlol.csv',attachment_filename='testlol.csv')
+    #return render_template('downloads.html')
+    return "Downloading"
 
 
 if __name__ == "__main__":
