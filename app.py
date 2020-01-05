dda08fa61ae0c619c72732de4baf86fc0abb433e
Blizzardi <40835275+Blizzardi@users.noreply.github.com>
2020-01-05 10:07:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,75 +1,92 @@
-from flask import Flask,render_template
-from flask import send_file,send_from_directory
-import os
+from flask import Flask,render_template,send_file
 import requests
+import os
 import csv
 import json
-import sys
+#import urllib.request
 
-app = Flask(__name__)
+
+
+app = Flask(__name__,template_folder='templates')
 
 @app.route('/')
 def index():
-    #return render_template('index.html')
-    return "hey"
+    return render_template('index.html')
 
-@app.route('/return-file/')
-def return_file():
-    #return send_file('/Users/shahzinsajid/SmartServProjects/jsontocsvconverted.csv',attachment_filename='jsontocsvconverted.csv')
-    return "hey"
-
-    
 @app.route('/jsonParser')
-def jsonParser(methods=['GET', 'POST']):
-    
-    return "successfully downloaded"
-
+def jsonParser():
     download_url = "https://open-to-cors.s3.amazonaws.com/users.json"
+    #response = urllib.request.urlopen("https://open-to-cors.s3.amazonaws.com/users.json")
+    #html = response.read()
+
     r = requests.get(download_url)
+    #with open("testerss.json","wb") as f:
+      #  f.write(r.content)
 
-    with open("testerss.json","wb") as f:
-        f.write(r.content)
-
-    with open('testerss.json') as f:
-        x = json.load(f)
-
-    f = csv.writer(open("jsontocsvconverted.csv", "w", newline=''))
-
-    f.writerow(["id", "phones", "email", "firstname", "lastname","role","username","isActive","_created_at","_updated_at"])
-
+   # with open('testerss.json') as f:
+    x = json.loads(r.content)
+   # return x
+    #f = csv.writer(open("jsontocsvconverted.csv", "w", newline=''))
+    
+    
+    outputArray =  ["id", "phones", "email", "firstname", "lastname","role","username","isActive","_created_at","_updated_at"]
+    output = ','.join(outputArray)
+    i = 0
+    #return output
     for x in x:
+        #try:
+            #return x["id"]
         try:
-            f.writerow([x["id"],
-                        x["phones"],
-                        x["email"],
-                        x["firstname"],
-                        x["lastname"],
-                        x["role"],
-                        x["username"],
-                        x["isActive"],
-                        x["_created_at"],
-                        x["_updated_at"]])
+            idValue = x["id"] or x["_id"]
+           # if type(idValue) == <type 'int'>:
+            idValue = str(idValue)
         except:
-            f.writerow([x["_id"],
-                        x["phones"],
-                        x["email"],
-                        x["firstname"],
-                        x["lastname"],
-                        x["role"],
-                        x["username"],
-                        x["isActive"],
-                        x["_created_at"],
-                        x["_updated_at"]])
+            idValue = x["_id"] or "nil"
+
+        output2 =   ",".join([ idValue ,
+                        x["phones"] or "nil",
+                        x["email"] or "nil",
+                        x["firstname"] or "nil",
+                        x["lastname"] or "nil",
+                        str(x["role"]) or "nil",
+                        x["username"] or "nil",
+                       str(x["isActive"]) or "nil" ,
+                        
+                        str(x["_created_at"]) or "",
+                        str(x["_updated_at"]) or ""]) 
+            
+        #output = output + "\n' + output2 
+
+        output =  output + "<br>" + output2 #'\n'.join([output, output2])
+        i = i + 1
+            
+          
+       # except:
+                #return x["_id"]
+                #return  ",".join([x["_id"],
+                        #x["phones"] or "",
+                        #x["email"] or "",
+                        #x["firstname"] or "",
+                        #x["lastname"] or "",
+                        #x["role"] or "",
+                        #x["username"] or "",
+                        #x["isActive"] or "",
+                        #x["_created_at"] or ""
+                       # x["_updated_at"] or "" ]
+                        #])
 
     #return send_from_directory(directory='/smartservprojects',filename='testlol.csv', as_attachment=True)
     #return "downloading.."
     #return send_file('/Users/shahzinsajid/SmartServProjects/testlol.csv',attachment_filename='testlol.csv')
-    return render_template('downloads.html')
+    #return render_template('downloads.html')
+    return output
+    #return render_template('downloads.html')
+    #return "hey"
 
+@app.route('/return-file/')
+def return_file():
+    return send_file('/Users/shahzinsajid/SmartServProjects/jsontocsvconverted.csv',attachment_filename='jsontocsvconverted.csv')
+    #return "hey"
 
 if __name__ == "__main__":
-    #port = int(os.environ.get("PORT", 5000))
-    #app.run(host='0.0.0.0', port=port)
-    #print('hello', file=sys.stderr)
-    app.run()
-    
+    app.run(debug=True,threaded=True)
